794970a26c8a9d2423ad8ef48999e2f4c7c3337a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-05-01 12:07:43 +0200

    chore: add some Code Schemas

--- src/schemez/__init__.py
+++ src/schemez/__init__.py
@@ -2,5 +2,6 @@ __version__ = "0.1.0"
 
 
 from schemez.schema import Schema
+from schemez.code import PythonCode, JSONCode, TOMLCode, YAMLCode
 
-__all__ = ["Schema"]
+__all__ = ["JSONCode", "PythonCode", "Schema", "TOMLCode", "YAMLCode"]
