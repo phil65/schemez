fee37ec89e8a6be1d64c50ee82879e5d51df1ef6
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 05:25:01 +0200

    fix: resolve __init__.py merge conflict from template update

--- src/schemez/__init__.py
+++ src/schemez/__init__.py
@@ -1,27 +1,5 @@
-__version__ = "0.2.5"
+from __future__ import annotations
 
+from importlib.metadata import version
 
-from schemez.schema import Schema
-from schemez.code import PythonCode, JSONCode, TOMLCode, YAMLCode
-from schemez.schemadef.schemadef import (
-    SchemaDef,
-    SchemaField,
-    ImportedSchemaDef,
-    InlineSchemaDef,
-)
-from schemez.pydantic_types import ModelIdentifier, ModelTemperature, MimeType
-
-__all__ = [
-    "ImportedSchemaDef",
-    "InlineSchemaDef",
-    "JSONCode",
-    "MimeType",
-    "ModelIdentifier",
-    "ModelTemperature",
-    "PythonCode",
-    "Schema",
-    "SchemaDef",
-    "SchemaField",
-    "TOMLCode",
-    "YAMLCode",
-]
+__version__ = version("schemez")
