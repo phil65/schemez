1b99cef47ee81affdbc5abf7e5af57f19094f66e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:37:59 +0200

    build: bump template

--- src/schemez/__init__.py
+++ src/schemez/__init__.py
@@ -1,6 +1,17 @@
+"""Schemez: Pydantic shim for config stuff"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
+
+__version__ = version("schemez")
+__title__ = "Schemez"
+__description__ = "Pydantic shim for config stuff"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/schemez"
 
 from schemez.schema import Schema
 from schemez.code import PythonCode, JSONCode, TOMLCode, YAMLCode
